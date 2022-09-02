1df30241c376e5cb434f3fba1a375c20a83988a4
Chloe <ama1049@nyu.edu>
2022-09-02 15:40:56 -0400

    New test code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 #include <string>
 #include "tcp.h"
 using namespace std;
+using namespace cppsock;
 
+
+//Server
+/*
 int main(int argc, char *argv[]){
     if(argc != 2){
         cout << "Usage: " << argv[0] << " <port>" << endl;
@@ -16,5 +20,23 @@ int main(int argc, char *argv[]){
     string msg = server.sock_receive();
     cout << msg << endl;
     server.disconnect();
+    return 0;
+}
+*/
+
+//Client
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        cout << "Usage: " << argv[0] << " <ip> <port>" << endl;
+        return 1;
+    }
+    string ip = argv[1];
+    int port = atoi(argv[2]);
+    TCPClient client(ip, port);
+    client.sock_connect();
+    client.sock_send("Hello World");
+    string msg = client.sock_receive();
+    cout << msg << endl;
+    client.disconnect();
     return 0;
 }
