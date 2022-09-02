e4ee9d3cc9755031ae9e243457aba6996fbab9ef
Chloe <ama1049@nyu.edu>
2022-09-02 18:33:15 -0400

    Windows UDP Impl (untested)

--- udp.cpp
+++ udp.cpp
@@ -1,16 +1,31 @@
 #include "udp.h"
+using namespace std;
+using namespace cppsock;
 
 //UNTESTED
 UDPClient::UDPClient(string ip, int port){
+    #ifdef _WIN32
+    WSAnumInstances++;
+    WSAManage();
+    //TODO: get raw address
+    #endif
     this->ip = ip;
     this->port = port;
     this->connected = false;
 }
 UDPClient::~UDPClient(){
+    #ifdef __linux__
     close(sock);
+    #endif
+    #ifdef _WIN32
+    closesocket(sock);
+    WSAnumInstances--;
+    WSAManage();
+    #endif
 }
 bool UDPClient::sock_connect(){
     if(this->connected) return true;
+    #ifdef __linux__
     this->sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(this->sock < 0){
         cout << "Error creating socket" << endl;
@@ -24,42 +39,96 @@ bool UDPClient::sock_connect(){
         return false;
     }
     this->connected = true;
+    #endif
+    #ifdef _WIN32
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(sock == INVALID_SOCKET){
+        cout << "Error creating socket" << endl;
+        return false;
+    }
+    //Connect via IP
+    this->server.sin_family = AF_INET;
+    this->server.sin_port = htons(this->port);
+    this->server.sin_addr.s_addr = inet_addr(this->ip.c_str());
+    if(connect(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR){
+        cout << "Error connecting to server" << endl;
+        return false;
+    }
+    this->connected = true;
+    #endif
     return true;
 }
 bool UDPClient::sock_send(string msg){
     if(!this->connected) return false;
+    #ifdef __linux__
     if(sendto(this->sock, msg.c_str(), msg.length(), 0, (struct sockaddr *)&this->server, sizeof(this->server)) < 0){
         cout << "Error sending message" << endl;
         return false;
     }
+    #endif
+    #ifdef _WIN32
+    if(sendto(sock, msg.c_str(), msg.length(), 0, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR){
+        cout << "Error sending message" << endl;
+        return false;
+    }
+    #endif
     return true;
 }
 string UDPClient::sock_receive(){
     if(!this->connected) return "";
+    //zero out buffer
+    memset(this->buffer, 0, sizeof(this->buffer));
+    #ifdef __linux__
     socklen_t server_len = sizeof(this->server);
     if(recvfrom(this->sock, this->buffer, 1024, 0, (struct sockaddr *)&this->server, &server_len) < 0){
         cout << "Error receiving message" << endl;
         return "";
     }
+    #endif
+    #ifdef _WIN32
+    int server_len = sizeof(this->server);
+    if(recvfrom(sock, this->buffer, 1024, 0, (struct sockaddr *)&this->server, &server_len) == SOCKET_ERROR){
+        cout << "Error receiving message" << endl;
+        return "";
+    }
+    #endif
     return this->buffer;
 }
 
 bool UDPClient::disconnect(){
     if(!this->connected) return false;
+    #ifdef __linux__
     close(this->sock);
     this->connected = false;
+    #endif
+    #ifdef _WIN32
+    closesocket(this->sock);
+    this->connected = false;
+    #endif
     return true;
 }
 
 UDPServer::UDPServer(int port){
+    #ifdef _WIN32
+    WSAnumInstances++;
+    WSAManage();
+    #endif
     this->port = port;
     this->connected = false;
 }
 UDPServer::~UDPServer(){
+    #ifdef __linux__
     close(sock);
+    #endif
+    #ifdef _WIN32
+    closesocket(sock);
+    WSAnumInstances--;
+    WSAManage();
+    #endif
 }
 bool UDPServer::sock_listen(){
     if(this->connected) return true;
+    #ifdef __linux__
     this->sock = socket(AF_INET, SOCK_DGRAM, 0);
     if(this->sock < 0){
         cout << "Error creating socket" << endl;
@@ -73,6 +142,22 @@ bool UDPServer::sock_listen(){
         return false;
     }
     this->connected = true;
+    #endif
+    #ifdef _WIN32
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if(sock == INVALID_SOCKET){
+        cout << "Error creating socket" << endl;
+        return false;
+    }
+    this->server.sin_family = AF_INET;
+    this->server.sin_port = htons(this->port);
+    this->server.sin_addr.s_addr = INADDR_ANY;
+    if(bind(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR){
+        cout << "Error binding socket" << endl;
+        return false;
+    }
+    this->connected = true;
+    #endif
     return true;
 }
 
@@ -83,26 +168,51 @@ bool UDPServer::sock_accept(){
 
 bool UDPServer::sock_send(string msg){
     if(!this->connected) return false;
+    #ifdef __linux__
     if(sendto(this->sock, msg.c_str(), msg.length(), 0, (struct sockaddr *)&this->server, sizeof(this->server)) < 0){
         cout << "Error sending message" << endl;
         return false;
     }
+    #endif
+    #ifdef _WIN32
+    if(sendto(sock, msg.c_str(), msg.length(), 0, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR){
+        cout << "Error sending message" << endl;
+        return false;
+    }
+    #endif
     return true;
 }
 
 string UDPServer::sock_receive(){
     if(!this->connected) return "";
+    //zero out buffer
+    memset(this->buffer, 0, sizeof(this->buffer));
+    #ifdef __linux__
     socklen_t server_len = sizeof(this->server);
     if(recvfrom(this->sock, this->buffer, 1024, 0, (struct sockaddr *)&this->server, &server_len) < 0){
         cout << "Error receiving message" << endl;
         return "";
     }
+    #endif
+    #ifdef _WIN32
+    int server_len = sizeof(this->server);
+    if(recvfrom(sock, this->buffer, 1024, 0, (struct sockaddr *)&this->server, &server_len) == SOCKET_ERROR){
+        cout << "Error receiving message" << endl;
+        return "";
+    }
+    #endif
     return this->buffer;
 }
 
 bool UDPServer::disconnect(){
     if(!this->connected) return false;
+    #ifdef __linux__
     close(this->sock);
     this->connected = false;
+    #endif
+    #ifdef _WIN32
+    closesocket(sock);
+    this->connected = false;
+    #endif
     return true;
 }
--- udp.h
+++ udp.h
@@ -1,5 +1,6 @@
 #ifndef UDP_H
 #define UDP_H
+#define BUF_SIZE 1024
 #include <iostream>
 #include <string>
 //if Linux
@@ -13,44 +14,56 @@
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include "windows_overhead.h"
 #pragma comment(lib, "Ws2_32.lib")
 #endif
 
 using namespace std;
 
-class UDPClient{
-    private:
-        int sock;
-        struct sockaddr_in server;
-        char buffer[1024];
-        string ip;
-        string hostname;
-        int port;
-        bool connected;
-    public:
-        UDPClient(string ip, int port);
-        ~UDPClient();
-        bool sock_connect();
-        bool sock_send(string msg);
-        string sock_receive();
-        bool disconnect();
-};
+namespace cppsock{
+    class UDPClient{
+        private:
+            #ifdef __linux__
+            int sock;
+            #endif
+            #ifdef _WIN32
+            SOCKET sock = INVALID_SOCKET;
+            #endif
+            struct sockaddr_in server;
+            char buffer[BUF_SIZE];
+            string ip;
+            string hostname;
+            int port;
+            bool connected;
+        public:
+            UDPClient(string ip, int port);
+            ~UDPClient();
+            bool sock_connect();
+            bool sock_send(string msg);
+            string sock_receive();
+            bool disconnect();
+    };
 
-class UDPServer{
-    private:
-        int sock;
-        struct sockaddr_in server;
-        char buffer[1024];
-        int port;
-        bool connected;
-    public:
-        UDPServer(int port);
-        ~UDPServer();
-        bool sock_listen();
-        bool sock_accept();
-        bool sock_send(string msg);
-        string sock_receive();
-        bool disconnect();
-};
-
+    class UDPServer{
+        private:
+            #ifdef __linux__
+            int sock;
+            #endif
+            #ifdef _WIN32
+            SOCKET sock = INVALID_SOCKET;
+            #endif
+            struct sockaddr_in server;
+            char buffer[BUF_SIZE];
+            int port;
+            bool connected;
+        public:
+            UDPServer(int port);
+            ~UDPServer();
+            bool sock_listen();
+            bool sock_accept();
+            bool sock_send(string msg);
+            string sock_receive();
+            bool disconnect();
+    };
+}
 #endif // UDP_H
